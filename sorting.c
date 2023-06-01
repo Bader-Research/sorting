959f7c53368210f40d1497d124233568b9a88def
David Bader <dbader13@gmail.com>
2023-05-31 20:27:37 -0400

    Added timSort

--- sorting.c
+++ sorting.c
@@ -1621,6 +1621,89 @@ void mergeSort2_nr(int* arr, int n) {
 
 /*************************************************************************/
 
+// Minimum size of a run
+#define MIN_RUN 32
+
+// Function to perform insertion sort
+void insertionSort_timsort(int* arr, int left, int right) {
+  for (int i = left + 1; i <= right; i++) {
+    int key = arr[i];
+    int j = i - 1;
+        
+    while (j >= left && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+        
+    arr[j + 1] = key;
+  }
+}
+
+// Function to merge two runs
+void mergeRuns(int* arr, int left, int mid, int right) {
+  int len1 = mid - left + 1;
+  int len2 = right - mid;
+    
+  int* leftArr = (int*)malloc(len1 * sizeof(int));
+  int* rightArr = (int*)malloc(len2 * sizeof(int));
+    
+  for (int i = 0; i < len1; i++) {
+    leftArr[i] = arr[left + i];
+  }
+    
+  for (int i = 0; i < len2; i++) {
+    rightArr[i] = arr[mid + 1 + i];
+  }
+    
+  int i = 0, j = 0, k = left;
+    
+  while (i < len1 && j < len2) {
+    if (leftArr[i] <= rightArr[j]) {
+      arr[k] = leftArr[i];
+      i++;
+    }
+    else {
+      arr[k] = rightArr[j];
+      j++;
+    }
+    k++;
+  }
+    
+  while (i < len1) {
+    arr[k] = leftArr[i];
+    i++;
+    k++;
+  }
+    
+  while (j < len2) {
+    arr[k] = rightArr[j];
+    j++;
+    k++;
+  }
+    
+  free(leftArr);
+  free(rightArr);
+}
+
+// Function to perform Timsort
+void timSort(int* arr, int n) {
+  for (int i = 0; i < n; i += MIN_RUN) {
+    insertionSort_timsort(arr, i, (i + MIN_RUN - 1 < n - 1) ? (i + MIN_RUN - 1) : (n - 1));
+  }
+    
+  for (int size = MIN_RUN; size < n; size = 2 * size) {
+    for (int left = 0; left < n; left += 2 * size) {
+      int mid = left + size - 1;
+      int right = (left + 2 * size - 1 < n - 1) ? (left + 2 * size - 1) : (n - 1);
+            
+      mergeRuns(arr, left, mid, right);
+    }
+  }
+}
+
+
+/*************************************************************************/
+
 
 void create_input(DATA_TYPE *list, int elems) {
   int i;
@@ -2168,7 +2251,7 @@ main(int argc, char **argv) {
 
 /******************************************************************/
 
-  /******************************************************************/
+/******************************************************************/
 
   total_time = get_seconds();
   for (loop=0 ; loop<LOOP_CNT ; loop++) {
@@ -2189,6 +2272,31 @@ main(int argc, char **argv) {
   total_time /= (double)LOOP_CNT;
 
   fprintf(stdout," elems: %12d \t    mSort_nr: %f\n",
+	  elems,total_time);
+
+/******************************************************************/
+
+/******************************************************************/
+
+  total_time = get_seconds();
+  for (loop=0 ; loop<LOOP_CNT ; loop++) {
+    bcopy(originalList,list,elems*sizeof(DATA_TYPE));
+    timSort(list, elems);
+  }
+  total_time = get_seconds() - total_time;
+  err = check_sort(list,elems);
+  if (!err) fprintf(stderr,"ERROR with timSort\n");
+
+  over_time = get_seconds();
+  for (loop=0 ; loop<LOOP_CNT ; loop++) {
+    bcopy(originalList,list,elems*sizeof(DATA_TYPE));
+  }
+  over_time = get_seconds() - over_time;
+
+  total_time -= over_time;
+  total_time /= (double)LOOP_CNT;
+
+  fprintf(stdout," elems: %12d \t     timSort: %f\n",
 	  elems,total_time);
 
 /******************************************************************/
